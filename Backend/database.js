de86afeee041b87af7be6df3db982f0489d9e4df
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2023-08-05 13:35:09 +0530

    Update database.js

--- Backend/database.js
+++ Backend/database.js
@@ -1,7 +1,7 @@
 import mysql from 'mysql2'
 import 'dotenv/config'
 
-export const db = mysql.createConnection({
+export const db = mysql.createConnection({ 
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
