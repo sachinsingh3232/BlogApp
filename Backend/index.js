24b4ed72254f48a14a71115b2a1d3c398b6fe2cb
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2023-08-05 19:59:19 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,10 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+    origin: process.env.CORS_URL,
+    credentials: true
+}));
 
 app.use('/api/posts', postRoute);
 app.use('/api/auth', authRoute);
