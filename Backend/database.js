a37b0d23ab77c33c8cbeb66937c4c75a29f5e1d5
SACHIN SINGH <92503784+sachinsingh3232@users.noreply.github.com>
2023-08-05 16:04:25 +0530

    Update database.js

--- Backend/database.js
+++ Backend/database.js
@@ -6,5 +6,5 @@ export const db = mysql.createConnection({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_DATABASE,
-    port: process.env.DATABASE_PORT
+    port: process.env.DATABASE_PORT 
 })
